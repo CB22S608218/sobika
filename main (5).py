7c39f2447987528415e3a353e0a2b94a4002cea1
CB22S608218 <144780414+CB22S608218@users.noreply.github.com>
2023-09-25 10:55:42 +0530

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,22 @@
+'''linear_Search_Product'''
+
+def linerSearchProduct(productList,  targetProduct):
+   indices = []
+  
+   for index, product in enumerate(productList):
+     if product == targetProduct:
+      indices.append(index)
+
+   return indices
+
+#Example usage:
+products = ["shoes", "boot","Loafer","shoes","sandal","shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linerSearchProduct(products, target)
+print(result)
+ 
+
+
+
+
